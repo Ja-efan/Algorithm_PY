2831989cd12a4cf06c9f73f57b049ba81130dcf5
Jaehwan <woghks1213@gmail.com>
2020-09-08 17:00:26 +0900

    add files01

---
+++ Ch01/median3.py
@@ -0,0 +1,27 @@
+def med3(a,b,c):
+    if a>=b:
+        if b>=c:
+            return b
+
+        elif a<=c:
+            return a
+
+        else:
+             return c
+
+    elif a > c: # 'a < b' 가 전제 조건
+        return a
+
+    elif b > c : # 'a < b' 가 전제 조건
+        return c
+
+    else : # 'a < b' 가 전제 조건 & 'b <= c'
+        return b
+
+
+print("세 정수의 중앙값을 구합니다.")
+a = int(input("정수 a의 값을 입력:"))
+b = int(input("정수 b의 값을 입력:"))
+c = int(input("정수 c의 값을 입력:"))
+
+print(f'중앙값은 {med3(a,b,c)}입니다.')
